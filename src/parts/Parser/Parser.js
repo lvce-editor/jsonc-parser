3a33df1b6a605190b6e5920d10ac4f351723a7c7
Le Vivilet <levivlet@pm.me>
2024-06-07 14:13:58 +0200

    feature: move parse functions into separate files

--- src/parts/Parser/Parser.js
+++ src/parts/Parser/Parser.js
@@ -1,21 +1,10 @@
 import * as TokenType from '../JsoncTokenType/JsoncTokenType.js'
-import * as Literal from '../Literal/Literal.js'
-
-const parsePropertyName = (scanner) => {
-  const propertyName = scanner.scanPropertyName()
-  return propertyName
-}
-
-const parsePropertyColon = (scanner) => {
-  scanner.scanPropertyColon()
-}
-
-const parseNumber = (scanner) => {
-  scanner.goBack(1)
-  const rawValue = scanner.scanNumber()
-  const value = parseFloat(rawValue)
-  return value
-}
+import * as ParseComment from '../ParseComment/ParseComment.js'
+import * as ParseLiteral from '../ParseLiteral/ParseLiteral.js'
+import * as ParseNumber from '../ParseNumber/ParseNumber.js'
+import * as ParsePropertyColon from '../ParsePropertyColon/ParsePropertyColon.js'
+import * as ParsePropertyName from '../ParsePropertyName/ParsePropertyName.js'
+import * as ParseString from '../ParseString/ParseString.js'
 
 const parseObject = (scanner) => {
   const object = {}
@@ -28,29 +17,23 @@ const parseObject = (scanner) => {
         break outer
       case TokenType.DoubleQuote:
         scanner.goBack(1)
-        const propertyName = parsePropertyName(scanner)
-        parsePropertyColon(scanner)
+        const propertyName = ParsePropertyName.parsePropertyName(scanner)
+        ParsePropertyColon.parsePropertyColon(scanner)
         const value = parseValue(scanner)
         object[propertyName] = value
       case TokenType.Comma:
         break
       case TokenType.Slash:
-        parseComment(scanner)
+        ParseComment.parseComment(scanner)
         break
       case TokenType.Literal:
-        parseLiteral(scanner)
+        ParseLiteral.parseLiteral(scanner)
         break
       default:
         break
     }
   }
   return object
-}
-
-const parseString = (scanner) => {
-  scanner.goBack(1)
-  const value = scanner.scanString()
-  return value
 }
 
 const parseArray = (scanner) => {
@@ -77,39 +60,21 @@ const parseArray = (scanner) => {
   return array
 }
 
-const parseLiteral = (scanner) => {
-  const rawValue = scanner.scanLiteral()
-  switch (rawValue) {
-    case Literal.True:
-      return true
-    case Literal.False:
-      return false
-    case Literal.Null:
-      return null
-    default:
-      return undefined
-  }
-}
-
-const parseComment = (scanner) => {
-  scanner.scanComment()
-}
-
 export const parseValue = (scanner) => {
   const token = scanner.scanValue()
   switch (token) {
     case TokenType.CurlyOpen:
       return parseObject(scanner)
     case TokenType.DoubleQuote:
-      return parseString(scanner)
+      return ParseString.parseString(scanner)
     case TokenType.Numeric:
-      return parseNumber(scanner)
+      return ParseNumber.parseNumber(scanner)
     case TokenType.SquareOpen:
       return parseArray(scanner)
     case TokenType.Literal:
-      return parseLiteral(scanner)
+      return ParseLiteral.parseLiteral(scanner)
     case TokenType.Slash:
-      parseComment(scanner)
+      ParseComment.parseComment(scanner)
       return parseValue(scanner)
     default:
       return undefined
