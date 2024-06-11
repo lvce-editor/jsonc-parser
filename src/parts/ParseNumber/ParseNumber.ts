becb66cf72fe41fa505fc251895d4817e47b32ee
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-11 07:48:33 +0200

    feature: lint code with xo (#30)

--- src/parts/ParseNumber/ParseNumber.ts
+++ src/parts/ParseNumber/ParseNumber.ts
@@ -3,6 +3,6 @@ import type { Scanner } from '../Scanner/Scanner.ts'
 export const parseNumber = (scanner: Scanner) => {
   scanner.goBack(1)
   const rawValue = scanner.scanNumber()
-  const value = parseFloat(rawValue)
+  const value = Number.parseFloat(rawValue)
   return value
 }
